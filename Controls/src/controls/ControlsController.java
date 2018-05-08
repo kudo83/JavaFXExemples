42335781ec86e828579d0084a6e5d5c35fec6546
Aissam IZIMI <ai.izimi@gmail.com>
2018-05-08 16:29:01 +0100

    F

--- Controls/src/controls/ControlsController.java
+++ Controls/src/controls/ControlsController.java
@@ -5,12 +5,13 @@
  */
 package controls;
 
+import com.jfoenix.controls.JFXComboBox;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
 
 /**
  *
@@ -18,18 +19,21 @@ import javafx.scene.control.Label;
  */
 public class ControlsController implements Initializable {
     
-    @FXML
-    private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private ToggleGroup genderGroupe;
+    @FXML
+    private JFXComboBox<String> educationCombo;
+   
+    
+       @FXML
+    private void handleRegistration(ActionEvent event) {
+        //Code to complete registration.
     }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        educationCombo.getItems().addAll("Elementary school", "Middle school" , "High school", "College");
+    }  
 }
